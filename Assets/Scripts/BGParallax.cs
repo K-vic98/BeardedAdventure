82828698678df2bb3ecaf8c68c75ace5fcbc4af8
alexey lysenko <abesmon@gmail.com>
2021-02-09 18:09:25 +0300

    parallax feature
    parallax feature

---
+++ Assets/Scripts/BGParallax.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class BGParallax : MonoBehaviour
+{
+    [SerializeField] private Vector2 _mult;
+    [SerializeField] private GameObject _trackObj;
+
+    private Vector3 _anchorPointStartPos;
+    private SpriteRenderer _spriteRenderer;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _anchorPointStartPos = _trackObj.transform.position;
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 distanceFromOriginToTrack = _trackObj.transform.position - _anchorPointStartPos;
+        Vector2 shift = distanceFromOriginToTrack * _mult;
+        _spriteRenderer.sharedMaterial.SetVector("_Offset", shift);
+    }
+}
